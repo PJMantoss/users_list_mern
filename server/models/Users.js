77f42f0f7b60b467ce46f8949117626841d0174f
PJMantoss <joelptoss@gmail.com>
2022-09-21 17:41:20 +0100

    update Users model

--- server/models/Users.js
+++ server/models/Users.js
@@ -5,6 +5,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    age: {},
+    age: {
+        type: Number,
+        required: true
+    },
     username: {}
 });
