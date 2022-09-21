29b116fc69604f8fdeb990de7c8d87844b127d18
PJMantoss <joelptoss@gmail.com>
2022-09-21 17:33:09 +0100

    add connection to mongoose in server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://mantoss:userlproject@cluster0.v1krpfd.mongodb.net/test");
+mongoose.connect("mongodb+srv://mantoss:userlproject@cluster0.v1krpfd.mongodb.net/userslist?retryWrites=true&w=majority");
 
 app.listen(3001, () => {
     console.log('SERVER IS RUNNING!')
