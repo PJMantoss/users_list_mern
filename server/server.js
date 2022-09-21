c170645823f5e55df67721d85580770af0ac475f
PJMantoss <joelptoss@gmail.com>
2022-09-21 17:48:50 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://mantoss:userlproject@cluster0.v1krpfd.mongodb.net/userslist?retryWrites=true&w=majority");
 
+app.get()
+
 app.listen(3001, () => {
     console.log('SERVER IS RUNNING!')
 });
