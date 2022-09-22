0ad7712fff98fbf43b303eb100c220ed4c10530b
PJMantoss <joelptoss@gmail.com>
2022-09-22 20:05:58 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ const UserModel = require("./models/Users");
 mongoose.connect("mongodb+srv://mantoss:userlproject@cluster0.v1krpfd.mongodb.net/userslist?retryWrites=true&w=majority");
 
 // API requests
-app.get("/getUsers", (req, res) => {})
+app.get("/getUsers", (req, res) => {
+    UserModel.find({});
+})
 
 app.listen(3001, () => {
     console.log('SERVER IS RUNNING!')
