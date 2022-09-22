9c90ec134f7b775dedde46574162b005f5cf28a0
PJMantoss <joelptoss@gmail.com>
2022-09-22 20:01:01 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -11,3 +11,5 @@ app.get("/getUsers", (req, res) => {})
 app.listen(3001, () => {
     console.log('SERVER IS RUNNING!')
 });
+
+// mongodb+srv://mantoss:userlproject@cluster0.v1krpfd.mongodb.net/test
