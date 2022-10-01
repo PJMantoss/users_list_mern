0ad09617232b9da1aa3509c0d8af70a74140d7eb
PJMantoss <joelptoss@gmail.com>
2022-10-01 19:19:07 +0100

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div className="usersDisplay">
-        {listOfUsers.map(() = > {})}
+        {listOfUsers.map(() => {})}
       </div>
     </div>
   );
