521c0bf723cff2509999db1113295aff5c6fd9c0
PJMantoss <joelptoss@gmail.com>
2022-10-01 19:15:08 +0100

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [] = useState([]);
+  
   return (
     <div className="App">
       <div className="usersDisplay"></div>
